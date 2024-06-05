15834c36eee42884ad33e753ef387e52f0faf097
romanpastu <roman.pastushenko.1997@gmail.com>
2024-06-06 00:16:29 +0200

    use pure mantine

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@mantine/core";
+import { Box, Button, ScrollArea, Text } from "@mantine/core";
 import { json, type MetaFunction } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { db } from "~/utils/db.server";
@@ -20,35 +20,49 @@ export default function Index() {
   const data = useLoaderData<typeof loader>();
 
   return (
-    <Box style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', height: "100vh" }}>
-      <Box style={{
-        border: "1px solid #000",
-        height: "5vh",
-        width: "70vw",
+    <Box
+      style={{
         display: 'flex',
+        flexDirection: 'column',
         alignItems: 'center',
-        justifyContent: 'center',
-
-      }}>
-        <p>No Weekly Task Selected</p>
+        height: '100vh',
+      }}
+    >
+      <Box
+        style={{
+          border: '1px solid #000',
+          height: '5vh',
+          width: '70vw',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Text>No Weekly Task Selected</Text>
       </Box>
 
-      <Box style={{
-        width: "70vw",
-        overflowY: "scroll",
-        flex: 1,
-        boxSizing: 'border-box'
-      }}>
+      <ScrollArea
+        style={{
+          width: '70vw',
+          flex: 1,
+        }}
+      >
         {data.taskItems.map((task) => (
-          <Box key={task.id}
+          <Box
+            key={task.id}
             style={{
-              border: "1px solid #000",
-              width: "100%",
-            }}>
-            {task.title}
+              border: '1px solid #000',
+              width: '100%',
+              marginTop: '8px',
+            }}
+          >
+            <Box style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', padding: '8px' }}>
+              <Text>{task.title}</Text>
+              <Button>Set weekly</Button>
+            </Box>
           </Box>
         ))}
-      </Box>
+      </ScrollArea>
     </Box>
   );
 }

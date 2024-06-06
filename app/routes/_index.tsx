ac958be72d647e9981e1633ddbfd5aa475e92c01
romanpastu <roman.pastushenko.1997@gmail.com>
2024-06-06 23:52:27 +0200

    some refactor

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,6 @@
 import { Box, ScrollArea, Text } from "@mantine/core";
+import { Task } from "@prisma/client";
+
 import { json, type MetaFunction } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { TASK_STATUS } from "~/constants/tasks";
@@ -9,6 +11,19 @@ export const meta: MetaFunction = () => {
     { title: "New Remix App" },
     { name: "description", content: "Welcome to Remix!" },
   ];
+}
+
+type TaskWithDateString = Omit<Task, 'createdAt' | 'updatedAt'> & {
+  createdAt: string;
+  updatedAt: string;
+};
+
+const convertTaskDates = (task: TaskWithDateString): Task => {
+  return {
+    ...task,
+    createdAt: new Date(task.createdAt),
+    updatedAt: new Date(task.updatedAt),
+  };
 };
 
 export const loader = async () => {
@@ -27,6 +42,47 @@ export const loader = async () => {
 
 export default function Index() {
   const data = useLoaderData<typeof loader>();
+
+  const doingTasks = data.doingTasks.map(convertTaskDates);
+  const doneTasks = data.doneTasks.map(convertTaskDates);
+  const pendingTasks = data.pendingTasks.map(convertTaskDates);
+
+  const renderTasks = (tasks: Task[]) => {
+    return tasks.length === 0 ? (
+      <Box
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          border: '1px solid black',
+          padding: '8px',
+          width: '65vw',
+        }}
+      >
+        <Text>Weekly tasks</Text>
+      </Box>
+    ) : (
+
+      tasks.map((task: Task) => (
+        <Box
+          key={task.id}
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            border: '1px solid black',
+            padding: '8px',
+            width: '65vw',
+            marginTop: '4px',
+            marginBottom: '4px',
+          }}
+        >
+          <Text>{task.title}</Text>
+        </Box>
+      ))
+    );
+  };
+
   return (
     <Box
       style={{
@@ -34,6 +90,7 @@ export default function Index() {
         flexDirection: 'column',
         alignItems: 'center',
         height: '100vh',
+        padding: '20px',
       }}
     >
       <Box
@@ -43,44 +100,37 @@ export default function Index() {
           display: 'flex',
           alignItems: 'center',
           justifyContent: 'center',
-          flexDirection: "column"
+          flexDirection: 'column',
+          padding: '16px',
         }}
       >
-        {
-          [...data.doingTasks, ...data.doneTasks].length === 0 ?
-            (
-              <Box style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', border: "1px solid black", padding: "8px", width: "65vw" }}>
-                <Text>Weekly tasks</Text>
-              </Box>
-            )
-            :
-            (
-              [...data.doingTasks, ...data.doneTasks].map((task) => (
-                <Box key={task.id} style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', border: "1px solid black", padding: "8px", width: "65vw", marginTop: '4px', marginBottom: "4px" }}>
-                  <Text>{task.title}</Text>
-                </Box>
-              ))
-            )
-        }
-
+        {renderTasks([...doingTasks, ...doneTasks])}
       </Box>
 
       <ScrollArea
         style={{
           width: '70vw',
           flex: 1,
+          marginTop: '16px',
         }}
       >
-        {data.pendingTasks.map((task) => (
+        {pendingTasks.map((task) => (
           <Box
             key={task.id}
             style={{
               border: '1px solid #000',
               width: '100%',
               marginTop: '8px',
+              padding: '8px',
             }}
           >
-            <Box style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', padding: '8px' }}>
+            <Box
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+              }}
+            >
               <Text>{task.title}</Text>
             </Box>
           </Box>

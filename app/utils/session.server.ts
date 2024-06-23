9fe931d47acabee553a08d4b4f670c31403944fd
romanpastu <roman.pastushenko.1997@gmail.com>
2024-06-23 22:48:39 +0200

    disable secure cookies

--- app/utils/session.server.ts
+++ app/utils/session.server.ts
@@ -55,12 +55,12 @@ import {
       // normally you want this to be `secure: true`
       // but that doesn't work on localhost for Safari
       // https://web.dev/when-to-use-local-https/
-      secure: process.env.NODE_ENV === "production",
+      secure: false,
       secrets: [sessionSecret],
       sameSite: "lax",
       path: "/",
       maxAge: 60 * 60 * 24 * 30,
-      httpOnly: true,
+      httpOnly: false,
     },
   });
   
